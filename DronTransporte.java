ab4e7715176f6a865aaced7cace5b8925de08df6
Lucas Monges <saculmonges@gmail.com>
2026-04-19 10:54:10 -0300

    Change patronMovimiento to return a string

--- DronTransporte.java
+++ DronTransporte.java
@@ -1,19 +1,18 @@
 package ProyectoNieves;
 
- class DronTransporte extends Vehiculo implements IConectable{
-     public DronTransporte(String id){
+ class DronTransporte extends Vehiculo implements IConectable {
+     public DronTransporte(String id) {
          super(id);
          System.out.println("Vehiculo:Dron de Transporte");
      }
 
      @Override
-     public void patronMovimiento() {
-         System.out.println("Patrón detectado: alabeo,cabeceo,guiñada,aceleración");
+     public String patronMovimiento() {
+         return "Patrón detectado: alabeo,cabeceo,guiñada,aceleración";
      }
 
      @Override
      public void sincronizarGPS() {
-         System.out.println("GPS Sincronizado correctamente ");
+         System.out.println("GPS Sincronizado correctamente");
      }
-
  }
